72f8bf6d2b074f9f431c8f1675baad9740d9d76c
경재한 <201901651@inu.ac.kr>
2023-02-22 17:40:37 +0900

    connect mongodb

--- gif-chat/gif-chat/app.js
+++ gif-chat/gif-chat/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const flash = require('connect-flash');
+const ColorHash = require('color-hash').default;
 require('dotenv').config();
 
 const webSocket = require('./socket');
@@ -12,6 +13,16 @@ const connect = require('./schemas');
 
 const app = express();
 connect();
+
+const sessionMiddleware = session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -22,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(sessionMiddleware);
 app.use(session({
     resave: false,
     saveUninitialized: false,
@@ -32,6 +44,14 @@ app.use(session({
     },
 }));
 app.use(flash());
+
+app.use((req, res, next) => {
+    if (!req.session.color) {
+        const colorHash = new ColorHash();
+        req.session.color = colorHash.hex(req.sessionID);
+    }
+    next();
+});
 
 app.use('/', indexRouter);
 
@@ -52,4 +72,4 @@ const server = app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
 });
 
-webSocket(server);
+webSocket(server, app, sessionMiddleware);
